6473c63612d468b5e686d27d76a32595b1d6164e
Shriram <shriram@houseofbluebeans.com>
2019-02-20 16:03:39 +0530

    Added Forms & Scene Changes
    #Developer: Shriram

---
+++ assets/js/app1.js
@@ -0,0 +1,21 @@
+//we can set animation delay as following in ms (default 1000)
+ProgressBar.singleStepAnimation = 500;
+ProgressBar.init(
+  [ 'Received by Hobb',
+    'Awaiting additional inputs',
+    'Work initiated',
+    'Asset creation in progress',
+    'Scene setup in progress',
+    'Drafts shared',
+    'Feedback pending',
+    'Iteration in progress',
+    'Iterations Shared',
+    'Feedback pending',
+    'Final in progress',
+    'Final Shared',
+    'Final delivered',
+    'Completed'
+  ],
+  'Final Shared',
+  'progress-bar-wrapper3' // created this optional parameter for container name (otherwise default container created)
+);
---
+++ assets/js/asset-app2.js
@@ -0,0 +1,23 @@
+//we can set animation delay as following in ms (default 1000)
+ProgressBar.singleStepAnimation = 500;
+ProgressBar.init(
+  [ 'Received by Hobb',
+    'Awaiting additional inputs',
+    'Work initiated',
+    'Model creation in progress',
+    'Texture and material creation in progress',
+    'Low fidelity Asset completed',
+    'High fidelity Asset completed',
+    'Silos In Progress',
+    '360º Spins in Progress',
+    'Silo/360º Drafts shared',
+    'Feedback pending',
+    'Iteration in progress',
+    'Feedback pending',
+    'Final in progress',
+    'Final delivered ',
+    'Completed'
+  ],
+  'Feedback pending',
+  'progress-bar-wrapper2' // created this optional parameter for container name (otherwise default container created)
+);
